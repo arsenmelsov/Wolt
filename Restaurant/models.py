a9ab5cffdd92e72d9d1120e71a3b578280499d77
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 02:44:06 +0500

    created Restaurant category

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from City.models import City
+import uuid
 
-# Create your models here.
+class Restaurant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.name
