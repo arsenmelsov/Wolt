fd53e8134cf27585ec350241978cab5a62eda6c5
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 01:30:42 +0500

    created UserCard model

--- UserCard/models.py
+++ UserCard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Users.models import User
+from Card.models import Card
+import uuid
 
-# Create your models here.
+class UserCard(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
