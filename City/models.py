fde2a29c56159f82e534987c2127973b2e2a1746
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 02:37:51 +0500

    created City model

--- City/models.py
+++ City/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Country.models import Country
+import uuid
 
-# Create your models here.
+class City(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.name
