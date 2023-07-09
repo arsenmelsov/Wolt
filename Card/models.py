fd1f7c3059f1175ce42ae50d9c09211a2988114a
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 01:26:48 +0500

    created Card model

--- Card/models.py
+++ Card/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Card(models.Model):
+    number = models.TextField(null=False)
+    date = models.TextField(null=False)
+    cvv = models.TextField(null=False)
+
+    def str(self):
+        return self.name
