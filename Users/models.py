031b41cb7469c20a1455ef3afbb5035af880dd95
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 00:55:15 +0500

    created User model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    surname = models.CharField(max_length=50, null=False)
+    phone_number = models.TextField(null=False)
+    email = models.EmailField(null=False)
+    password = models.CharField(max_length = 50, null=False)
+
+    def str(self):
+        return self.name
