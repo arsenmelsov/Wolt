b2e040adcccc1f4d937c07d21bf4277cd126ecc3
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 02:35:21 +0500

    created Country model

--- Country/models.py
+++ Country/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+
+    def str(self):
+        return self.name
--- UserCard/models.py
+++ UserCard/models.py
@@ -7,3 +7,6 @@ class UserCard(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.name
