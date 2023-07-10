1ba4f0ad14832c3637434a716fd76b57b3b4a573
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 17:11:19 +0500

    created ReccomendedFood model

--- ReccomendedFood/models.py
+++ ReccomendedFood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Food.models import Food
+import uuid
 
-# Create your models here.
+class ReccomendedFood(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.name
+
