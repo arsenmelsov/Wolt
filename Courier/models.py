99cbde122ee24aa8a750df0e64f825c940511841
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 16:25:49 +0500

    Created Courier model

--- Courier/models.py
+++ Courier/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Courier(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    phone_number = models.TextField(null=False)
+
+    def str(self):
+        return self.name
