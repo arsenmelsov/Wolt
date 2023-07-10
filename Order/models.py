4882e7f8486ae5b220105db62747830d20b827e9
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 17:15:10 +0500

    created Order model

--- Order/models.py
+++ Order/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Food.models import Food
+from Basket.models import Basket
+import uuid
 
-# Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
