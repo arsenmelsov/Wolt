9e8e1f8fda3ed5f8b73e24a7e12f054ac389f74d
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 16:42:58 +0500

    created BranchAddress model

--- Basket/models.py
+++ Basket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Courier.models import Courier
+import uuid
 
 class Basket(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
