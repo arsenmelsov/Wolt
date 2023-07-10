620cb668c2cae6cce4519f0a6f0fccb9c38b6599
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 16:38:53 +0500

    created Basket model

--- Basket/models.py
+++ Basket/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from Courier.models import Courier
 
-# Create your models here.
+class Basket(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    cost = models.IntegerField()
+    arrival_time = models.TimeField()
+    rating = models.FloatField()
+    payment_status = models.BooleanField(null=False)
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
--- Card/models.py
+++ Card/models.py
@@ -5,6 +5,3 @@ class Card(models.Model):
     number = models.TextField(null=False)
     date = models.TextField(null=False)
     cvv = models.TextField(null=False)
-
-    def str(self):
-        return self.name
