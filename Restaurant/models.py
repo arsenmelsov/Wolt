d94331d8cc5176d962dc455705947c04d6d72938
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 16:48:54 +0500

    added rest_category to Restaurant

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from City.models import City
+from Category.models import Category
 import uuid
 
 class Restaurant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     name = models.CharField(max_length=50, null=False)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+    rest_category = models.ManyToManyField(Category)
 
     def str(self):
         return self.name
