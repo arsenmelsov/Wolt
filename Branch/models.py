666dedbb84819090139ebac3ef562241f8011384
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 16:58:36 +0500

    created Branch model

--- Branch/models.py
+++ Branch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Restaurant.models import Restaurant
+from BranchAddress.models import BranchAddress
+import uuid
 
-# Create your models here.
+class Branch(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    radius = models.IntegerField()
+    rating = models.FloatField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    address = models.ForeignKey(BranchAddress, on_delete=models.CASCADE)
--- Country/models.py
+++ Country/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class Country(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
