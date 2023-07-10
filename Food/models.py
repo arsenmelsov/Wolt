0f7e97a64bf82186e77bf6f677b0b8abb4d5b049
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 18:50:35 +0500

    changed mistake in Food model

--- Food/models.py
+++ Food/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from Branch.models import Branch
 import uuid
 
-class User(models.Model):
+class Food(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     name = models.CharField(max_length=50, null=False)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
