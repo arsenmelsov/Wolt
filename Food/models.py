0afebd766f383ac2a6055d6a7df78437dc4e5417
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 17:03:56 +0500

    created Food model

--- Food/models.py
+++ Food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Branch.models import Branch
+import uuid
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=50, null=False)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.name
