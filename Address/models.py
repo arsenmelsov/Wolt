90de555890a42271060a445f027d3474a89661b9
arsenmelsov <arsmlsn21@gmail.com>
2023-07-10 17:29:20 +0500

    created Address model

--- Address/models.py
+++ Address/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from City.models import City
+from HouseType.models import houseType
+import uuid
 
-# Create your models here.
+class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    street = models.CharField(max_length=50, null=False)
+    house = models.CharField(max_length=50, null=False)
+    apartment = models.IntegerField()
+    entrance = models.IntegerField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    house_type = models.ForeignKey(houseType, on_delete=models.CASCADE)
+
+
+
